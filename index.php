f5c170f731df8747aeb173b034589105f82b4ed7
unknown <jdmontesino@sena.edu.co>
2021-07-23 11:11:34 -0500

    Cargue inicial

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title> Login </title>
+<meta http-equiv="Content-Type" content="text/html;  charset=utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<link href="modos.css" rel="stylesheet"/>
+</head>
+<body>
+   <div id="div1" class="container" >
+<br>
+   <div id="div2">
+   <p style="text-align: center; "><img src="mode.jpg"  class="rounded-circle"  width="800" height="200"><img src="mode.jpg"  class="rounded-circle"  width="800" height="200">
+<div id="div3">
+
+<form id="formulario" method="post" action="menu.php">
+ <br>
+ <p style="text-align:center ">
+ <strong class="lgris" style="font-size:200% "> Ingrese su usuario y contraseña para iniciar sesion</strong>
+<br>
+<em><label class="lgris"> Nombre de Usuario: </label></em>
+ <br>
+<button><input class="cambio" style="text-transform: uppercase;" type= "text"  name="usuario" value="" required/></button>
+ <br>
+<em><label class="lgris">Contraseña: </label></em>
+ <br>
+<button><input  class="espacio" type="password" name="clave" value="" required/></button>
+ <br><br>
+<button><input class="botoniniciar" type="submit" style="font-size:300%" value="iniciar sesion"></button>
+ <br><br>
+</p>
+</form>
+
+</div>
+</div>
+</div>
+<br><br>
+</body>
+</html>
